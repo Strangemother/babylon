2ca78c0ea25da4c708266bf3bf000a7a6ad4e3a0
Jay Jagpal <jay@strangemother.com>
2016-10-02 19:54:36 +0100

    rename target canvas

--- js-v2/config.js
+++ js-v2/config.js
@@ -2,7 +2,10 @@ CONFIG = {
     name: 'apos-app'
 
     // name of the canvas to implement
-    , canvasName: '#game_canvas'
+    , canvasName: '#renderCanvas'
     // width height of the canvas
     , size: [800, 600]
+
+    // relative from the HTML page.
+    , loadPath: './js-v2'
 }
