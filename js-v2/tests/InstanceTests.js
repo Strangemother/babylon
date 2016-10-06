e2e899c704cf77e0dd9ca6955e1a5e773558f92b
Jay Jagpal <jay@strangemother.com>
2016-10-06 01:34:10 +0100

    add comment to test

--- js-v2/tests/InstanceTests.js
+++ js-v2/tests/InstanceTests.js
@@ -5,6 +5,7 @@ var test = Test.assertions;
 class InstanceTests {
 
     test_exists() {
+        /* Ensure INSTANCE exists another large content block.*/
         test.value(INSTANCE).isObject()
     }
 }
