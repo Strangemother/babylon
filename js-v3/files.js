1282fb0370df6d875170fb6643b6b81c87083490
Jay Jagpal <jay@strangemother.com>
2017-01-15 22:08:36 +0000

    file cleanup

--- js-v3/files.js
+++ js-v3/files.js
@@ -3,27 +3,23 @@ var assets = [
    "js/babylon-2-5.custom.js"
    , 'js/vendor/IT.js'
    , 'js/vendor/color.js'
-
-   // Lib
-   , 'js-v3/lib/core/utils.js'
-
+   , 'js-v3/vendor/vue.min.js'
    , 'js-v2/vendor/jquery-3.1.1.min.js'
-
-   // , 'js-v2/tests/InstanceTests.js'
    , 'js-v2/vendor/mocha/mocha.js'
    , 'js-v2/vendor/unitjs/browser/dist/unit.js'
+
+   , 'js-v3/lib/core/utils.js'
    , 'js-v3/lib/tests.js'
 
    , 'js-v3/lib/PrintLogger.js'
    // App
    , 'js-v3/config.js'
-   // , 'js-v2/printLogger.js'
    , 'js-v3/lib/base.js'
    , 'js-v3/lib/adapter.js'
    , 'js-v3/lib/core/liveProperties.js'
    , 'js-v3/lib/mesh.js'
-   , 'js-v2/colors.js'
-   , 'js-v2/colors_extended.js'
+   , 'js-v3/lib/colors.js'
+   , 'js-v3/lib/colors_extended.js'
    , 'js-v3/lib/shapes.js'
    , 'js-v3/lib/lights.js'
    , 'js-v3/lib/cameras.js'
