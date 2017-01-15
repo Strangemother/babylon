35e02eba17733096162667d0357c7ac3a9f1affc
Jay Jagpal <jay@strangemother.com>
2017-01-15 22:09:23 +0000

    color vie

--- js-v3/main.js
+++ js-v3/main.js
@@ -4,6 +4,53 @@ var main = function(){
     window.app = v;
     $('#run_tests').click(runTests);
 
+    window.colorView = new Vue({
+        el: '.panel.colors'
+        , data: {
+            colors: colors.names
+            , colorStyle: {
+                backgroundColor: 'red'
+            }
+            , lightContrastValue: 130
+            , redMultiplier: 100
+            , greenMultiplier: 587
+            , blueMultiplier: 144
+            , isClosed: true
+        }
+        , methods: {
+            toggleIsOpen() {
+                this.isClosed = !this.isClosed
+            }
+            , contrastValue(name) {
+                var c = colors.get(name)
+                let [r,g,b] = [c.r, c.g, c.b].map(x => x * 255)
+                var yiq = (
+                        (r * this.redMultiplier)
+                        + (g * this.greenMultiplier)
+                        + (b * this.blueMultiplier)
+                    ) / 1000;
+                return yiq;
+            }
+
+            , lightContrast: function(name){
+                let yiq = this.contrastValue(name)
+                return (yiq <= this.lightContrastValue) // ? 'dark' : 'light';
+            }
+            , hexColor: function(name){
+                let v = colors.get(name);
+                let hex = colors.rgbToHex.apply({}, [v.r, v.g, v.b])
+                // this.colorStyle.backgroundColor = hex;
+                return hex;
+            }
+            , sliderChange: function(event) {
+                let k = event.currentTarget.dataset.multiplier;
+                let v = event.currentTarget.value;
+                this[k] = v;
+                console.log('sliderchange', event, v)
+
+            }
+        }
+    })
 }
 
 var runTests = function(){
