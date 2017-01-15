68450edae7675c7741b266c1f612f04995bc838d
Jay Jagpal <jay@strangemother.com>
2017-01-15 14:53:17 +0000

    Extended color property, made Position property.

--- js-v3/lib/core/liveProperties.js
+++ js-v3/lib/core/liveProperties.js
@@ -3,16 +3,55 @@ class ColorProperty extends BaseProperty {
         return 'color'
     }
 
+    instanceTypes() {
+        return [
+            Shape
+            , Light
+            , BabylonObject
+            ]
+    }
+
     initProperty(item, obj, options){
-        console.log('init color')
+        /* Init the property will generate the Color type before
+        the instance is BABYLON created.
+        setProperty applies the cached Color
+        to the material. */
         return [this.name, colors[options[this.name]]()]
     }
 
-    afterProperty(babylon, instance, key, properties){
-        let scene = instance._app.scene()
-        babylon.material = materials.color(scene, properties[key])
+
+    setProperty(instance, key, value, babylon) {
+        return this.Shape_setProperty(instance, key, value, babylon)
     }
 
+    Shape_setProperty(instance, key, value, babylon) {
+        let scene = instance._app.scene()
+        babylon = babylon == undefined? instance._babylon: babylon;
+        let c = colors.get(value);
+        instance._properties[key] = c
+        return babylon.material = materials.color(scene, c)
+    }
+
+    Light_setProperty(instance, key, value, babylon) {
+        babylon = babylon == undefined? instance._babylon: babylon;
+        let c = colors.get(value)
+        instance._properties[key] = c
+        babylon.diffuse = c
+        return c;
+    }
 }
 
-Garden.register(ColorProperty)
+class PositionProperty extends BaseProperty {
+    setProperty(instance, key, value, babylon) {
+        babylon = babylon == undefined? instance._babylon: babylon;
+        babylon.position = value;
+        return value;
+    }
+
+    getProperty(instance, key, value, babylon) {
+        babylon = babylon == undefined? instance._babylon: babylon;
+        return babylon.position
+    }
+}
+
+Garden.register(ColorProperty, PositionProperty)
