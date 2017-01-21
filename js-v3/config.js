de2c4de4448b446333c96a332a285307a49060d5
Jay Jagpal <jay@strangemother.com>
2017-01-21 13:20:03 +0000

    neater config

--- js-v3/config.js
+++ js-v3/config.js
@@ -1,8 +1,9 @@
 CONFIG = {
-    name: 'apos-app'
+    // name: 'apos-app'
 
-    // name of the canvas to implement
-    , canvasName: '#renderCanvas'
-    // width height of the canvas
-    , size: [1000, 800]
+    /* name of the canvas to implement */
+    canvasName: '#renderCanvas'
+
+    /* width height of the canvas */
+    // , size: [1000, 800]
 }
