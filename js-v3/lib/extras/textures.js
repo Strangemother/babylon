977f7d948660b72d11315639a86110c4b6e09a6d
Jay Jagpal <jay@strangemother.com>
2017-01-22 20:11:18 +0000

    Texture class

---
+++ js-v3/lib/extras/textures.js
@@ -0,0 +1,109 @@
+textures = {};
+textures.DIFFUSE = 'diffuseTexture'
+textures.EMISSIVE = 'emissiveTexture'
+
+
+class Texture extends BabylonObject {
+    static assignmentName(item) {
+        return 'textures'
+    }
+
+    keys(){
+        return [
+            'assetName'
+            , 'assetPath'
+        ]
+    }
+
+    assetPathKey(){
+        return "./assets/textures/"
+    }
+
+    makePath(n, p){
+        return `${p}${n}`
+    }
+
+    executeBabylon(babylonFunc, funcName, uniqueName, assetName, assetPath, scene, ...args) {
+        let p = this.makePath(assetName, assetPath)
+        return new babylonFunc[funcName](p, scene);
+    }
+
+    asMaterial(options, scene, type=textures.EMISSIVE){
+        /*
+        s = new Sphere;
+        m = s.create({ position: [0, 1, 0]});
+        t = new Texture;
+        tm = t.asMaterial({ assetName: '1.jpg' })
+        m.material = tm
+         */
+        options = options || this._options || {}
+        scene = scene || options.scene || this._app.scene();
+
+        let mat = this.makeMaterial(options, scene, type);
+
+        if(options.scene) {
+            delete options.scene
+        };
+
+        mat[type] = this.create(options, scene, false);
+        return mat
+    }
+
+    makeMaterial(options, scene, type=textures.EMISSIVE) {
+        let mat = materials.standard(scene);
+        return mat;
+    }
+
+    addToMesh(mesh, config, scene) {
+        let mat = this.asMaterial(config, scene)
+        mesh.material = mat
+        return mat
+    }
+
+}
+
+class SkyMaterial extends Texture {
+    /*
+        b = new SkyMaterial({assetName: 'mountains'})
+        box = new Box({ size: 100 })
+        mesh = box.addToScene()
+        mesh.material = c
+
+     */
+    babylonFuncName(){
+        return 'CubeTexture'
+    }
+
+    texturePathKey(optionValue, options, scene){
+        let pn = this.babylonFuncName()
+        let bp = this.getTextureBasePath()
+        return `${bp}${pn}/`
+    }
+
+    babylonExecuted(texture, name, assetName, assetPath, scene, ...args) {
+
+        let mat = materials.standard()
+        mat.backFaceCulling = false
+        mat.disableLighting = true
+        mat.diffuseColor = colors.black()
+        mat.specularColor = colors.black()
+
+        let p = this.makePath(assetName, assetPath)
+
+        mat.reflectionTexture = texture
+        mat.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE
+
+        return mat
+    }
+
+    makePath(n, p){
+        n = n || this._options.assetName
+        p = p || this._options.assetPath
+        let pn = this.babylonFuncName()
+        p = p || this.assetPathKey()
+        return `${p}${pn}/${n}/`
+    }
+
+}
+
+Garden.register(Texture)
