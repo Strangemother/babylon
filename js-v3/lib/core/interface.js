bf7e669a19d0d19740fdc709716a099125b980c6
Jay Jagpal <jay@strangemother.com>
2017-01-23 23:32:38 +0000

    Refactor classes

--- js-v3/lib/core/interface.js
+++ js-v3/lib/core/interface.js
@@ -1,4 +1,101 @@
-class BabylonInterface extends BabylonBase {
+class TargetObjectAssignmentRegister extends BaseClass {
+
+    static make(type, options) {
+        /* Genetate an instance*/
+
+        if(arguments.length <= 1) {
+            options = type;
+            type = undefined;
+        };
+
+        // Options or default options
+        options = options || {};
+        // Given type or the options.type or DEFAULT
+        type = (type || options.type).toLowerCase()
+
+        if(options._type) {
+            delete options.type;
+        };
+
+        let named = this.assignmentName()
+        let location = Garden.instance()[named] || {};
+        let item = location[type].make(location[type], options);
+        Garden.instance()[named] = location
+        return item;
+    }
+
+    static create(type, options, scene) {
+        let item = this.make(type, options);
+        // The given scene or the options scene
+        scene = scene || (options != undefined? options.scene: undefined)
+
+        if(scene == undefined) {
+            let engine, canvas;
+            [scene, engine, canvas] = item._app.babylonSet;
+        };
+
+        // Create new mesh
+        let mesh = item.create(scene);
+        // Add to display list.
+        item._app.children.append(item, mesh, options)
+        return item;
+    }
+
+    static register(...items){
+        // let named = Garden.assignmentName();
+        let _instance = Garden.instance();
+        /* Register a component for later creation via MeshTool.make and .create*/
+        let location = {};
+        let name, camelName, aName, inst, instName
+
+        for(let item of items) {
+            inst = item.assignmentReference ? item.assignmentReference(item): item;
+            aName = item.assignmentName ? item.assignmentName(item): 'named';
+            location = _instance[aName] || location;
+            name = item.name.toLowerCase();
+            instName = inst.name || name;
+            camelName = `${name.slice(0,1).toLowerCase()}${name.slice(1)}`
+
+            if(location[instName] != undefined) {
+                console.warn('Overwriting', instName, 'on', location)
+            }
+
+            location[instName] = inst;
+
+            if(item.targetObjectAssignment) {
+                let n = item.targetObjectAssignment(inst, _instance)
+                if(n === undefined) {
+                    console.warn('targetObjectAssignment name was undefined for', inst);
+                }
+
+                if(_instance[n] == undefined) {
+                    _instance[n] = {}
+                };
+
+                _instance[n][instName] = inst;
+            }
+
+            _instance[aName] = location
+        };
+    }
+
+    static assignmentName(){
+        return 'named'
+    }
+}
+
+
+
+class BabylonInterface extends TargetObjectAssignmentRegister {
+
+    init(config){
+        log('BabylonBase init')
+        this._renderers = []
+        this.initConfig = config;
+        this.clearColor = [.3, .3, .3]
+        this._ran = false;
+        this._stop = false;
+    }
 
     shouldAttachCamera(){
         return true
@@ -12,14 +109,35 @@ class BabylonInterface extends BabylonBase {
 
         window.dispatchNativeEvent && dispatchNativeEvent('gamescenerun', this)
 
-        return super.runLoop(this._scene, engine);
+        return this._loop(this._scene, engine);
+    }
+
+    _loop(config, engine) {
+        engine = engine || this._engine;
+        var self = this;
+        this._ran = true;
+        if(engine) {
+            engine.runRenderLoop(function() {
+               self.renderLoop(config)
+            });
+        } else {
+            while(this._stop == false) {
+                this.renderLoop(config);
+            }
+        }
+
+        return true;
     }
 
     renderLoop(scene) {
         /* The render loop performs the scene.render() and steps the super(),
         calling each method in the render list.*/
         scene.render()
-        return super.renderLoop(scene)
+
+        for (var i = 0; i < this._renderers.length; i++) {
+            this._renderers[i](scene, i)
+        };
+
     }
 
     camera(scene, canvas){
@@ -115,6 +233,43 @@ class BabylonInterface extends BabylonBase {
         return engine;
     }
 
+    stop(v){
+        v = v === undefined? true: v;
+
+        if(v != undefined){
+            this._stop = v;
+        }
+
+        return this._stop;
+    }
+
+    config(v){
+        if(v) return this.totalConfig[v];
+
+        if(this.totalConfig == undefined && this._ran == false) {
+            console.warn('run() method requires call');
+            return Object.assign({}, this.initConfig || {}, this.runConfig || {});
+        }
+        return this.totalConfig;
+    }
+
+    getRunConfig(runConfig){
+        return Object.assign({},this.initConfig || {}, runConfig);
+    }
+
+    run(engine, runConfig) {
+
+        if(arguments.length == 1) {
+            runConfig = engine;
+            engine = undefined;
+        };
+
+        this.runConfig = runConfig || {};
+        this.totalConfig = this.getRunConfig(this.runConfig);
+        this.clearColor = this.config('backgroundColor') || this.clearColor
+        return this.runLoop(this.runConfig, this._engine)
+    }
+
     static handleWarning(errorId, message) {
         let n = `${errorId}::${message}`;
         console.warn(n)
@@ -192,5 +347,6 @@ class Base extends BabylonInterface {
         this.scene().clearColor = v;
         return true;
     }
+
 }
 
