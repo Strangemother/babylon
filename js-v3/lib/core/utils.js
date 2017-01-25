c0fd60111345162e9f0991295a7aadd4b5c95291
Jay Jagpal <jay@strangemother.com>
2017-01-25 18:28:04 +0000

    New ID generator to produce predicable ID

--- js-v3/lib/core/utils.js
+++ js-v3/lib/core/utils.js
@@ -219,10 +219,24 @@ var mergeOnKey = function(key, ...arrays) {
 }
 
 var simple_id_counter = 0
-var simpleID = function(optional=''){
-    let id = simple_id_counter++; // Math.random().toString(32).slice(2);
+var simpleID = function(optional='', counter=undefined){
+    let id = counter || simple_id_counter++; // Math.random().toString(32).slice(2);
     let insert = optional != ''? '_': '';
     return `${optional}${insert}${id}`
+}
+
+var complex_ids_counter = {
+    defCounter: 0
+}
+
+var complexID = function(optional='defCounter') {
+    if(complex_ids_counter[optional] == undefined) {
+        complex_ids_counter[optional] = 0
+    }
+
+    complex_ids_counter[optional]++;
+    return simpleID(optional, complex_ids_counter[optional]);
+
 }
 
 var asVector = function(...args){
