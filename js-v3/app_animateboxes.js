fbac333ac21a88a1f6c24d185d1581e76056db11
Jay Jagpal <jay@strangemother.com>
2017-01-26 22:58:54 +0000

    removed old function

--- js-v3/app_animateboxes.js
+++ js-v3/app_animateboxes.js
@@ -15,27 +15,6 @@ class AnimateBoxes extends Garden {
         this.camera = new app.cameras.ArcRotateCamera(true)
     }
 
-    destroyable(){
-        return [
-            this.skyBox
-            , this.skyBoxMesh
-            , this.camera
-            , this.box
-            , this.box2
-            , this.box3
-            , this.box4
-            , this.box5
-            , this.bAnim
-            , this.a2Anim
-            , this.a1Anim
-            , this.a4Anim
-            , this.animScaleY
-            , this.animScaleZ
-            , this.animScaleY
-            , this.animScaleX
-        ]
-    }
-
     animate1(){
 
         // Make a box and add it to the scene
