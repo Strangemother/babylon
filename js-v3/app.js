5d92c61b5000f9463feade2fc3c6e772ba46604c
Jay Jagpal <jay@strangemother.com>
2017-01-27 20:33:43 +0000

    Basic scene app

--- js-v3/app.js
+++ js-v3/app.js
@@ -309,6 +309,42 @@ class Particles extends Garden {
 }
 
 
+class BasicScene extends Garden {
+
+    start(){
+
+        this.cam = new FreeCamera({
+            activate: true
+            , position: asVector(0, 5, -10)
+            , target: asVector(0,0,0)
+        })
+
+        this.light = new HemisphericLight({
+            position: asVector(0, 1, 0)
+            , intensity: 1
+        })
+
+        this.sphere = new Sphere({
+            segments: 16
+            , diameter: 2
+            , position: asVector(0, 1, 0)
+        })
+
+        this.ground = new Ground({
+            width: 6
+            , height: 6
+            , subdivisions: 16
+        })
+
+        this.children.addMany(
+            this.cam
+            , this.light
+            , this.sphere
+            , this.ground
+        )
+    }
+}
+
 class ImmutableParticles extends Garden {
     start(){
         this.backgroundColor = colors.black()
@@ -404,6 +440,7 @@ Garden.register(Simple
                 , Sandbox
                 , ChildrenApp
                 , ShapeColumn
+                , BasicScene
                 , Particles
                 , ImmutableParticles
                 );
