9ff184ca09b9f2844df333f23740a7f356538c79
Jay Jagpal <jay@strangemother.com>
2017-01-28 18:07:51 +0000

    class file refactor

--- js-v3/app.js
+++ js-v3/app.js
@@ -68,46 +68,6 @@ class Main extends Garden {
 }
 
 
-class App extends Garden {
-    init(config){
-
-        config = config || {};
-        config.backgroundColor = [.2, .2, .4];
-        super.init(config);
-
-        this.run();
-    }
-
-    start(){
-
-        let b1 = new Sphere({
-            color: 'red'
-            , position: asVector(0, 1, 0)
-        });
-
-        let b2 = new Sphere({
-            color: 'dodgerBlue'
-            , position: asVector(0, 2, 0)
-        });
-
-        let b3 = new Sphere({
-            color: 'gold'
-            , position: asVector(0, 3, 0)
-        });
-
-        this.balls = [b1, b2, b3]
-        this.children.addMany(b1, b2, b3)
-
-        this._camera = new ArcRotateCamera();
-        this.light = new HemisphericLight({ color: 'white' });
-
-        this.sphere = new Sphere({ color: 'green' });
-        this.children.addMany(this.sphere, this.light);
-        this._camera.activate();
-    }
-}
-
-
 class Sandbox extends Garden {
     init(config){
         config = config || {};
@@ -150,34 +110,49 @@ class Sandbox extends Garden {
 }
 
 
-class ShapeColumn extends Garden {
+class SpotlightApp extends Garden {
+
     start(){
-        this._light = new HemisphericLight();
-        this._camera = new ArcRotateCamera({activate:true});
-        this.children.add(this._light);
+        this.backgroundColor = colors.black()
+        this._camera = new ArcRotateCamera({
+            activate:true
+            , radius: 20
+        });
 
-        let t = new app.shapes.TriangleLines({ color: 'green' });
-        let meshes = this.meshes = []
+        this.hemiLight = new HemisphericLight({
+            intensity: .4
+        })
 
-        let n = -Math.floor(colors.names.length/2), m;
-        for(let c of colors.names) {
-            m = t.create({
-                color: c
-                , position: [0, n++, 0]
-                , rotation: [0, (n++)*.01, 0]
-            })
-            meshes.push(m)
-        }
-    }
+        this.spotLight = new SpotLight({
+            position: asVector(0, 10, 0)
+            , direction: asVector(0, -1, 0)
+            , diffuse: 'white'
+            , angle: 1
+            , exponent: 3
+        })
 
-    _destroyable(){
-        return [
-            this._camera
-            , this._light
-            , this.meshes
-        ]
+        this.ground = new Ground({
+            color: 'lightBlue'
+            , width: 20
+            , height: 20
+            , position: [0, 0, 0]
+        })
+
+        this.box = new Box({
+            position: [0, 3, 0]
+            , color: 'yellow'
+        });
+
+        this.spotLight.shadow(this.box).receiver(this.ground)
+        this.children.addMany(this.spotLight, this.hemiLight, this.ground, this.box)
+
+        let anim = new Animation({ targetProperty: 'rotation.y' })
+        anim.frame(0, 1).frame(300, Math.PI * 1.99)
+        this.box.animate(anim)
+
     }
 }
+
 
 class ChildrenApp extends Garden {
     start(){
@@ -218,229 +193,10 @@ class ChildrenApp extends Garden {
 }
 
 
-class Particles extends Garden {
-    start(){
-        this._light = new PointLight({ color: 'white', intensity: 1});
-        this._camera = new ArcRotateCamera({
-            activate:true
-            , position: [0, 10, -50]
-        });
-
-        this.fact = 30;
-        this.children.add(this._light);
-
-        this.generate(this.options())
-    }
-
-    options(){
-        return {};
-    }
-
-    generate(options){
-        this.createParticleSystem(10000, options)
-    }
-
-    createParticleSystem(count=5000, options){
-        let model = this.modelParticle()
-        let scene = this.scene()
-        let sps = this.particleSystem(model, count, scene, options)
-        sps.initParticles = function(){
-            for (var i = 0; i < sps.nbParticles; i++) {
-                this.particleFunction(sps.particles[i])
-            }
-        }.bind(this);
-
-        var c = 0
-        sps.updateParticle = function (particle) {
-            particle.rotation.x += particle.position.z / 100;
-            particle.rotation.z += particle.position.x / 100;
-            //let fact = 30
-            if(c > colors.names.length-1) { c = 0}
-
-            //particle.color = colors.get(colors.names[c++])
-        }
-
-
-        sps.initParticles()
-        sps.setParticles()
-        //sps.computeParticleColor = false
-        sps.computeParticleTexture = false
-        let cp = app._camera._babylon.position
-        scene.registerBeforeRender(function() {
-            app._light.position = cp;
-
-            sps.mesh.rotation.y = -(cp.z/15) || 0
-            sps.mesh.rotation.z = -(cp.y/15) || 0
-            sps.setParticles();
-        });
-    }
-
-    particleFunction(particle, index, s){
-        let fact = this.fact;
-
-        particle.position.x = (Math.random() - 0.5) * fact;
-        particle.position.y = (Math.random() - 0.5) * fact;
-        particle.position.z = (Math.random() - 0.5) * fact;
-
-        particle.rotation.x = Math.random() * 3.15;
-        particle.rotation.y = Math.random() * 3.15;
-        particle.rotation.z = Math.random() * 1.5;
-        particle.color = colors.make(
-            particle.position.x / fact + 0.5
-            , particle.position.y / fact + 0.5
-            , particle.position.z / fact + 0.5
-            )
-    }
-
-    modelParticle() {
-        let d = new Disc({ tessellation: 3 })
-        return d.create();
-    }
-
-    particleSystem(shape, count=10000, scene, options){
-        let sps = new BABYLON.SolidParticleSystem('SPS', scene, options);
-        sps.addShape(shape, count)
-
-        let mesh = sps.buildMesh();
-        shape.dispose()
-        return sps
-    }
-
-}
-
-
-class BasicScene extends Garden {
-
-    start(){
-
-        this.cam = new FreeCamera({
-            activate: true
-            , position: asVector(0, 5, -10)
-            , target: asVector(0,0,0)
-        })
-
-        this.light = new HemisphericLight({
-            position: asVector(0, 1, 0)
-            , intensity: 1
-        })
-
-        this.sphere = new Sphere({
-            segments: 16
-            , diameter: 2
-            , position: asVector(0, 1, 0)
-        })
-
-        this.ground = new Ground({
-            width: 6
-            , height: 6
-            , subdivisions: 16
-        })
-
-        this.children.addMany(
-            this.cam
-            , this.light
-            , this.sphere
-            , this.ground
-        )
-    }
-}
-
-class ImmutableParticles extends Garden {
-    start(){
-        this.backgroundColor = colors.black()
-        this._light = new PointLight({
-            diffuse: 'white',
-            intensity: 2
-        });
-
-        this._camera = new ArcRotateCamera({
-            activate:true
-            , radius: 206
-            , position: [0, 116, -200]
-        });
-
-        this.fact = 90;
-        this.children.add(this._light);
-
-        this.generate()
-    }
-
-
-    generate(){
-        this.createParticleSystem(160000)
-    }
-
-    createParticleSystem(count){
-        let model = this.modelParticle()
-        let scene = this.scene()
-        let sps = this.particleSystem(model, count, scene)
-        sps.initParticles = function(){
-            for (var i = 0; i < sps.nbParticles; i++) {
-                this.particleFunction(sps.particles[i])
-            }
-        }.bind(this);
-
-        var c = 0
-
-        sps.initParticles()
-        sps.setParticles()
-        //sps.computeParticleColor = false
-        sps.computeParticleTexture = false
-        let cp =
-        scene.registerBeforeRender(function() {
-            app._light._babylon.position = app._camera._babylon.position;
-
-            sps.mesh.rotation.x -= .0001
-            sps.mesh.rotation.y += .0004
-            sps.mesh.rotation.z += .0001
-        });
-    }
-
-    particleFunction(particle, index, s){
-        let fact = this.fact;
-
-        particle.position.x = (Math.random() - 0.5) * fact;
-        particle.position.y = (Math.random() - 0.5) * fact;
-        particle.position.z = (Math.random() - 0.5) * fact;
-        particle.rotation.x = Math.random() * 3.15;
-        particle.rotation.y = Math.random() * 3.15;
-        particle.rotation.z = Math.random() * 1.5;
-
-        particle.color = colors.make(
-            particle.position.x / fact + 0.5
-            , particle.position.y / fact + 0.5
-            , particle.position.z / fact + 0.5
-            )
-    }
-
-    modelParticle() {
-        let d = new Disc({ tessellation: 3
-            , sideOrientation: BABYLON.Mesh.DOUBLESIDE
-        })
-        return d.create();
-    }
-
-    particleSystem(shape, count=10000, scene){
-        let sps = new BABYLON.SolidParticleSystem('SPS', scene, {updatable: false});
-        sps.addShape(shape, count, { positionFunction: this.particleFunction.bind(this)})
-
-        let mesh = sps.buildMesh();
-        shape.dispose()
-        return sps
-    }
-
-}
-
-
-
 Garden.register(Simple
                 , Blank
                 , Main
-                , App
                 , Sandbox
                 , ChildrenApp
-                , ShapeColumn
-                , BasicScene
-                , Particles
-                , ImmutableParticles
+                , SpotlightApp
                 );
