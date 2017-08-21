de6f3e5f67fc7b15d025718eaea4e432659e4ddb
Jay Jagpal <jay@strangemother.com>
2017-08-21 14:50:56 +0100

    An app presenting all basic elements

--- js-v3/apps/basic-elements.js
+++ js-v3/apps/basic-elements.js
@@ -107,4 +107,137 @@ class BasicElementsBabylonClone extends Garden {
     }
 }
 
-Garden.register(BasicElementsBabylonClone)
+class ElementCycle extends Garden {
+    items(){
+        let v = {
+            Box: {
+                size: 1.2
+            }
+
+            , Triangle: {
+                _item: 'Disc'
+                , radius: .8
+                , backFaceCulling: false
+                , tessellation: 3
+            }
+
+            , Plane: {
+                size: 1.2
+                , backFaceCulling: false
+            }
+
+            , DiscLess: {
+                _item: 'Disc'
+                , radius: .8
+                , tessellation: 8
+                , backFaceCulling: false
+            }
+            , Disc: {
+                radius: .8
+                , backFaceCulling: false
+            }
+
+            , Sphere: {
+                diameter: 1.3
+            }
+
+            , IcoSphere: {
+                radius: .7
+            }
+
+            , Cylinder: {
+                diameter: .8
+                , height: 1.2
+            }
+            , Cone: {
+                _item: 'Cylinder'
+                , diameter: 1
+                , height: 1.4
+                , diameterTop: 0
+            }
+            , TorusKnot: {
+                radius: .45
+                , tube: .12
+                , p: 2
+                , q: 3
+            }
+
+            , Torus: {
+                diameter: 1
+                , thickness: .3
+                , tessellation: 16
+            }
+        }
+
+        for (var i = 0; i < 14; i++) {
+            v[`Polyhedron_${i}`] = {
+                _item: 'Polyhedron'
+                , size: .6
+                , type: i
+            }
+        }
+
+        return v;
+    }
+
+    start() {
+        this.cam = new ArcRotateCamera({
+            activate: true
+             , alpha: 3 * Math.PI / 2
+             , beta: Math.PI / 8
+             , radius: 50
+             , target: asVector([0,0,0])
+         });
+
+        this.light = new HemisphericLight({
+            direction: asVector(0, 1, 0)
+        });
+
+        this.light.addToScene()
+
+        let items = this.items();
+        let count = Object.keys(items).length;
+        let r = {};
+        let inst, K;
+        let _counter = 0;
+        let y = 0, x = 0, i = 0;
+
+        for(let k in items) {
+
+            let name = k;
+            if(items[k]._item != undefined) {
+                name = items[k]._item;
+                delete items[k]._item
+            };
+
+            K = this.shapes[name];
+            i += 1;
+            _counter += 2;
+            x += 2
+
+            let _x = x - ((5 * 2) / 2)
+            let _y = y - ((5 * 2) / 2)
+            let position = asVector(_x, 0, _y);
+
+            if(i % 5 == 0) {
+                y += 2
+                x = 0;
+                _counter = 0
+            };
+
+            let color = 'lightBlue'
+            let conf = Object.assign({ color, position }, items[k])
+            inst = new K(conf);
+            inst.addToScene()
+            if(conf.backFaceCulling != undefined) {
+                inst.material().backFaceCulling = conf.backFaceCulling
+            }
+            if(r[k] == undefined) {
+                r[k] = []
+            }
+            r[k].push(inst)
+        }
+    }
+}
+
+Garden.register(BasicElementsBabylonClone, ElementCycle)
